dea33e10e6789188ed33e79e6e89dd04eacc35ea
ahmadtc1 <ahmadtc2000@gmail.com>
2019-10-17 18:40:11 -0400

    Perform face recognition given known and unknown faces
    Use face_recognition to extract face encodings and then compare face encodings to attempt to recognize faces in the unknown picture

---
+++ faceRecognitionSystem.py
@@ -0,0 +1,33 @@
+import face_recognition
+
+imageOne = face_recognition.load_image_file("person.jpg")
+imageTwo = face_recognition.load_image_file("personTwo.jpg")
+imageThree = face_recognition.load_image_file("personThree.jpg")
+
+personOneFaceEncoding = face_recognition.face_encodings(imageOne)[0]
+personTwoFaceEncoding = face_recognition.face_encodings(imageTwo)[0]
+personThreeFaceEncoding = face_recognition.face_encodings(imageThree)[0]
+
+knownFaceEncodings = [
+    personOneFaceEncoding, 
+    personTwoFaceEncoding, 
+    #personThreeFaceEncoding
+]
+
+unknownImage = face_recognition.load_image_file("unknown.jpg")
+
+unknownFaceEncodings = face_recognition.face_encodings(unknownImage)
+
+for unknownFaceEncoding in unknownFaceEncodings:
+    #Compare face encodings to determine whether the faces are match
+    results = face_recognition.compare_faces(knownFaceEncodings, unknownFaceEncoding)
+    name = "Unknown"
+    if results[0]:
+        name = "Person 1"
+    elif results[1]:
+        name = "Person 2"
+    elif results[2]:
+
+        name = "Person 3"
+
+    print("Found {} in the photo".format(name))
