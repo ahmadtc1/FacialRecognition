9aa656130efc9c4f4f0ec0dbc8cf44bdc03ed859
ahmadtc1 <ahmadtc2000@gmail.com>
2019-10-17 19:59:10 -0400

    Tune face recognition system to perform better with small images

--- faceRecognitionTuning.py
+++ faceRecognitionTuning.py
@@ -2,11 +2,11 @@ import face_recognition
 
 imageOne = face_recognition.load_image_file("person.jpg")
 imageTwo = face_recognition.load_image_file("personTwo.jpg")
-#imageThree = face_recognition.load_image_file("personThree.jpg")
+imageThree = face_recognition.load_image_file("personThree.jpg")
 
 personOneFaceEncoding = face_recognition.face_encodings(imageOne)[0]
 personTwoFaceEncoding = face_recognition.face_encodings(imageTwo)[0]
-#personThreeFaceEncoding = face_recognition.face_encodings(imageThree)[0]
+personThreeFaceEncoding = face_recognition.face_encodings(imageThree)[0]
 
 knownFaceEncodings = [
     personOneFaceEncoding, 
@@ -16,7 +16,9 @@ knownFaceEncodings = [
 
 unknownImage = face_recognition.load_image_file("unknown.jpg")
 
-unknownFaceEncodings = face_recognition.face_encodings(unknownImage)
+faceLocations = face_recognition.face_locations(unknownImage, number_of_times_to_upsample=2)
+
+unknownFaceEncodings = face_recognition.face_encodings(unknownImage, known_face_locations=faceLocations)
 
 for unknownFaceEncoding in unknownFaceEncodings:
     #Compare face encodings to determine whether the faces are match
@@ -26,7 +28,8 @@ for unknownFaceEncoding in unknownFaceEncodings:
         name = "Person 1"
     elif results[1]:
         name = "Person 2"
-    #elif results[2]:
-     #   name = "Person 3"
+    elif results[2]:
+
+        name = "Person 3"
 
     print("Found {} in the photo".format(name))
