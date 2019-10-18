eab5205c376a4e77a2cf724908b1e1c172b26d22
ahmadtc1 <ahmadtc2000@gmail.com>
2019-10-17 20:19:09 -0400

    Addding command line argument parsing
    Use argparse to have the user pass the filepath of the image when running the script
    :

--- faceLandmarks.py
+++ faceLandmarks.py
@@ -1,9 +1,20 @@
+#USAGE
+#python3 faceLandmarks.py --image person.jpg
+
 import PIL.Image
 import PIL.ImageDraw
 import face_recognition
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+    help="Path to the image to be landmarked"
+)
+args = vars(ap.parse_args())
 
 #Load the image into a numpy array
-image = face_recognition.load_image_file("faces.jpg")
+image = face_recognition.load_image_file(args["image"])
 
 #Find all the facial landmarks in the image using a face landmark detection algorithm
 faceLandmarksList = face_recognition.face_landmarks(image)
