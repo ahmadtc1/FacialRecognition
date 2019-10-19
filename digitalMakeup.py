2e79aedbec42497bfd1a96edf695da09f6e9baba
ahmadtc1 <ahmadtc2000@gmail.com>
2019-10-19 18:35:33 -0400

    Add digital makeup to faces using PIL library

---
+++ digitalMakeup.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw
+import face_recognition
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", 
+                required=True, 
+                help="Path to the image to be used"
+)
+args = vars(ap.parse_args())
+
+#Load the image as a np array and find the face landmarks in the picture
+image = face_recognition.load_image_file(args["image"])
+faceLandmarksList = face_recognition.face_landmarks(image)
+
+#Load the image so it can be drawn on using the PIL library
+pilImage = Image.fromarray(image)
+draw = ImageDraw.Draw(pilImage, 'RGBA')
+
+for faceLandmark in faceLandmarksList:
+    draw.line(faceLandmark["left_eyebrow"], fill=(128,0,128,100), width=7)
+    draw.line(faceLandmark["right_eyebrow"], fill=(128,0,128,100), width=7)
+    draw.polygon(faceLandmark["top_lip"], fill=(128,0,128,100))
+    draw.polygon(faceLandmark["bottom_lip"], fill=(128,0,128,100))
+
+pilImage.show()
